c6f4dc01f4039792839ecdecfa20b6973433510b
azizafarooq <azizafarooq2000@gmail.com>
2024-02-23 17:01:20 +0100

    feat: censored_word

--- result.php
+++ result.php
@@ -3,7 +3,8 @@ $paragraph = $_GET['paragrafo'];
 $censored_word = $_GET['parola-da-censurare'];
 
 $paragraph_length = strlen($paragraph);
-$censored_word = '***';
+
+$new_paragraph = str_replace($censored_word, '***', $paragraph);
 
 ?>
 <!DOCTYPE html>
@@ -22,6 +23,8 @@ $censored_word = '***';
         <h4><?php echo $paragraph?></h4>
         <h3>Lunghezza:</h2>
         <h4><?php echo $paragraph_length ?></h4>
+        <h3>Paragrafo censurato</h3>
+        <h4><?php echo $new_paragraph ?></h4>
     </div>
 </body>
 </html>
